c8c51a135f28cf239df2aa0773a3b01013f606cb
Laura <laura.ciocalau@gmail.com>
2022-12-21 21:16:47 +0100

    m

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ let unread = document.querySelectorAll('.unread')
 let button = document.querySelector("button")
 let notification = document.querySelectorAll('.notification')
 let newNotif = document.querySelectorAll('.newNotif')
-function read(){
-  if(unread){
-for(i=0; i < unread.length; i++)
-  dot[i].classList.remove('unread');
 
-}
-if(newNotif){
-for(i=0; i < unread.length; i++)
-notification[i].classList.remove('newNotif');
-}
-button.innerHTML = "0";
+function read() {
+  if (unread) {
+    for (i = 0; i < unread.length; i++)
+      dot[i].classList.remove('unread');
+  }
+  if (newNotif) {
+    for (i = 0; i < unread.length; i++)
+      notification[i].classList.remove('newNotif');
+  }
+  button.innerHTML = "0";
 }
